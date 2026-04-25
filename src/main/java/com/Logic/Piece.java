354cca036882c407ac5fa5761e5b091ebf487a3f
longlinh123456 <buiduylinh700@gmail.com>
2026-04-26 00:33:11 +0700

    scaffold base piece logic

--- src/main/java/com/Logic/Piece.java
+++ src/main/java/com/Logic/Piece.java
@@ -1,19 +1,45 @@
 package com.Logic;
 
-public class Piece {
-    private String type;
-    private String color;
+// A piece in the game
+public abstract class Piece {
+    public enum Color {
+        BLACK,
+        WHITE
+    }
+
+    private Color color;
+    // current location is tracked automatically to avoid boilerplate
     private int row;
     private int col;
 
-    public Piece(String type, String color, int row, int col) {
-        this.type = type;
+    public Piece(Color color, int row, int col) {
         this.color = color;
         this.row = row;
         this.col = col;
     }
-    
-    public String getImageFileName() {
-        return "piece_img/" + color.toLowerCase() + "_" + type.toLowerCase() + ".png";
+
+    // called by GameState to update the position of this piece
+    final public void updatePosition(int row, int col) {
+        this.row = row;
+        this.col = col;
     }
+    
+    public abstract String getImageFileName();
+    // user commands this piece to "move" to a square to the board
+    public abstract void move(GameState state, int row, int col);
+
+    // get the squares this piece can move to
+    // returns a 2D array of Strings that are filenames of images that should be overlaid on the movable squares
+    // array can contain nulls to indicate that the corresponding square cannot be moved to
+    public abstract String[][] getMovableSquares();
+
+    // called when this piece is displaced (moved adversarially) by another piece
+    // the previous location is passed to this function
+    public abstract void onDisplace(int prevRow, int prevCol);
+
+    // called when this piece's color is changed
+    public abstract void onColorChange();
+
+    // called when this piece is captured
+    public abstract void onCaptured();
 }
