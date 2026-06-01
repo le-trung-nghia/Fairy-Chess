b2acc86243e34ee7d9ddb1808fdaf081c37a6fc4
vinuni-nghia-le-trung <letrungnghiastudy@gmail.com>
2026-06-02 00:55:03 +0700

    Remove accidentally committed nested project folders from chessboard-gui/

--- chessboard-gui/chessboard-logic/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.chess.logic {
-    exports com.chess.logic.state;
-    exports com.chess.logic.types;
-    exports com.chess.registry;
-    exports com.chess.logic.boardregion;
-
-    requires transitive one.util.streamex;
-}
